82ef905f9bcce2c29d59950724321781ced68909
francisflow <Iamngigikaranja1@aol.com>
2022-10-03 21:37:43 +0300

    Styled the signup form to be centered and responsive

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
+import { Container } from "react-bootstrap";
 import Signup from "./authentication/Signup";
 
 function App() {
   return (
-    <Signup/>
+    <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh"}} >
+      <div className="w-100" style={{maxWidth:"400px"}}>
+                  <Signup/>
+
+      </div>
+
+    </Container>
   );
 }
 
